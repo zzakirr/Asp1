938cea6c10fbb0a6a3509ee94d8e4ff34efa0939
zzakirr <7nez6ws@code.edu.az>
2022-05-27 12:01:15 +0400

    commit file

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController:Controller
+    {
+        /*public ContentResult Index()
+        {
+            *//*return new ContentResult()
+            {
+                Content = "selam"
+            };*//*
+            return Content("selam");
+        }*/
+        public ViewResult Index()
+        {
+            var viewInd = new ViewResult();
+            viewInd.ViewName = "Index";
+            return viewInd;
+        }
+
+
+        public ViewResult About()
+        {
+            var view = new ViewResult();
+            view.ViewName = "About";
+            return view;
+        }
+    }
+}
